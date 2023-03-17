113562c23b636f6aa0403a7865cd1e2a079d67e0
will198735 <recinoswilfredo@gmail.com>
2023-03-17 01:23:20 -0700

    set up thought file model added util date format

---
+++ main/model/thought.js
@@ -0,0 +1,73 @@
+
+    const { Schema, model, Types } = require("mongoose");
+    const dateFormat = require("../utils/dateFormat");
+    
+const reactionSchema = new Schema ( {
+
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    require: true,
+    maxlength: 208,
+  },
+  username: {
+    type: String,
+    required: true,
+
+  },
+  createdAt:{
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+},
+{
+    toJson:{
+        getters: true,
+    },
+    id: false,
+
+}
+);
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText:{
+            type: String,
+            required: "Thought is required",
+            minlength: 1,
+            maxlength: 280,
+
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions:  [ReactionSchema],
+
+        
+    },
+    {
+        toJson:{
+           virtuals: true,
+           gethers: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
+  const Thought = model("Thought", thoughtSchema);
+  module.exports = Thought;
